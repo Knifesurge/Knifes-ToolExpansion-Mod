228dfc7ed042397a8f983cb9db94152addea05d0
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:05:08 -0500

    Create NetherStone.java

---
+++ block/NetherStone.java
@@ -0,0 +1,12 @@
+package com.knifesurge.ToolExpansion.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class NetherStone extends Block{
+
+	public NetherStone(Material materialIn) {
+		super(materialIn);
+	}
+
+}
