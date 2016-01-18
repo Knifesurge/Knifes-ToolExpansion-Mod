9ea764ad22b95ffd7aaf53592b3ebb9a8de0289e
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:15:21 -0500

    Create ObsidianSword.java

---
+++ items/tools/ObsidianSword.java
@@ -0,0 +1,11 @@
+package com.knifesurge.ToolExpansion.items.tools;
+
+import net.minecraft.item.ItemSword;
+
+public class ObsidianSword extends ItemSword {
+
+	public ObsidianSword(ToolMaterial material) {
+		super(material);
+	}
+
+}
