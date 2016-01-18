43fc890a68bbb7efc18af93f85fb0d547e1b4222
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:20:48 -0500

    Create Reference.java

---
+++ Reference.java
@@ -0,0 +1,10 @@
+package com.knifesurge.ToolExpansion;
+
+public class Reference {
+	public static final String MODID = "toolexpansion";
+	public static final String MODNAME = "Tool Expansion";
+	public static final String VERSION = "1.0";
+	public static final String CLIENT_PROXY_CLASS = "com.knifesurge.ToolExpansion.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.knifesurge.ToolExpansion.proxy.CommonProxy";
+	
+}
