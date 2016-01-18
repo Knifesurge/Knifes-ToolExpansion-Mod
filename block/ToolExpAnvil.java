f85478e77cdbc37452c1bf59be1102bada375a78
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:05:50 -0500

    Create ToolExpAnvil.java

---
+++ block/ToolExpAnvil.java
@@ -0,0 +1,12 @@
+package com.knifesurge.ToolExpansion.block;
+
+import net.minecraft.block.BlockAnvil;
+import net.minecraft.block.material.Material;
+
+public class ToolExpAnvil extends BlockAnvil {
+	
+	public ToolExpAnvil() {
+		
+	}
+	
+}
