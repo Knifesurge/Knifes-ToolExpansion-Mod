98c130914ba987a3e57f474af47522ce2237ccd7
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:15:44 -0500

    Create QuartzSword.java

---
+++ items/tools/QuartzSword.java
@@ -0,0 +1,11 @@
+package com.knifesurge.ToolExpansion.items.tools;
+
+import net.minecraft.item.ItemSword;
+
+public class QuartzSword extends ItemSword{
+
+	public QuartzSword(ToolMaterial material) {
+		super(material);
+	}
+
+}
