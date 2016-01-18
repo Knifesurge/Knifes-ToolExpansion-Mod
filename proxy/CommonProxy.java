362a19a1a34024b71f2d6f7d64e67738e83882da
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:18:28 -0500

    Create CommonProxy.java

---
+++ proxy/CommonProxy.java
@@ -0,0 +1,14 @@
+package com.knifesurge.ToolExpansion.proxy;
+
+public class CommonProxy {
+
+	public void registerRenders()
+	{
+		
+	}
+	
+	public int addArmor(String armor)
+	{
+		return 0;
+	}
+}
