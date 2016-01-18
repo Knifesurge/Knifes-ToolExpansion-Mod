3cb7e653bd814bf699c3f44c0cfbf7b351f86ce2
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:17:10 -0500

    Create CooledBlazeRod.java

---
+++ items/CooledBlazeRod.java
@@ -0,0 +1,16 @@
+package com.knifesurge.ToolExpansion.items;
+
+import com.knifesurge.ToolExpansion.init.ToolExpansionItems;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class CooledFireRod extends Item {
+
+	public CooledFireRod(){
+		
+	}
+
+}
