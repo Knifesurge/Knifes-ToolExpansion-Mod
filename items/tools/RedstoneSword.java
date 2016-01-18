0b9cb6d78578af14801efb214c1853b070ef19bc
Nick Mills <carlos94563@gmail.com>
2016-01-17 22:16:20 -0500

    Create RedstoneSword.java

---
+++ items/tools/RedstoneSword.java
@@ -0,0 +1,11 @@
+package com.knifesurge.ToolExpansion.items.tools;
+
+import net.minecraft.item.ItemSword;
+
+public class RedstoneSword extends ItemSword{
+
+	public RedstoneSword(ToolMaterial material) {
+		super(material);
+	}
+
+}
